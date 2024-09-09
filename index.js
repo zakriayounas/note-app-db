3cc02e4a1c59c6b2186b29f0cd642c93ca78acd5
Zakria Younas <zakriayounas01@gmail.com>
2024-09-09 16:55:05 +0500

    install nodemon

--- index.js
+++ index.js
@@ -27,5 +27,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+console.log(PORT);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
